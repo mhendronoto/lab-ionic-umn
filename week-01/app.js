0d217fd708083f01f9e927e345e88dc2c986952e
mhendronoto <slimenoto0611@gmail.com>
2019-08-21 19:42:15 +0700

    add week02

---
+++ week-01/app.js
@@ -0,0 +1,8 @@
+function add() {
+    var x = document.getElementById('x').value;
+    var y = document.getElementById('y').value;
+
+    return false;
+}
+
+document.getElementById('go').addEventListener('click', add);
